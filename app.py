46030c9701e6924f16be8a7c745fea608676a9d2
felipeprograms <cruzfelipe941@gmail.com>
2026-05-11 21:58:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from components.feed import render_feed
 from components.sidebar import render_sidebar
 
 setup_page()
+
+# Metatag de verificação Lomadee
+st.markdown('<meta name="lomadee" content="2324685" />', unsafe_allow_html=True)
+
 render_header()
 
 col_main, col_side = st.columns([3, 1])
